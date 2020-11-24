d1a40c9d831dc37dde365714567cc8b43e4d9175
janbernloehr <jan@bernloehrs.de>
2020-11-24 16:45:13 +0100

    Set ACTIONS_ALLOW_UNSECURE_COMMANDS

--- src/setup-catkin.ts
+++ src/setup-catkin.ts
@@ -3,7 +3,7 @@ import * as finder from './find-catkin';
 import child_process = require('child_process');
 
 function installRos(version: string) {
-  let command = `sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
+  let command = `export ACTIONS_ALLOW_UNSECURE_COMMANDS=true && sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 &&
 sudo apt-get update &&
 sudo apt-get -qq update -y &&
